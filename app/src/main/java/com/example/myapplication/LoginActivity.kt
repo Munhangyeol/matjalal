34918018347fe2d95e65f7e7ca9c6b9cfe0afc21
Munhangyeol <mhg10181018@gmail.com>
2024-01-24 14:39:55 +0900

    Login&SignupUI complete

--- app/src/main/java/com/example/myapplication/LoginActivity.kt
+++ app/src/main/java/com/example/myapplication/LoginActivity.kt
@@ -1,12 +1,24 @@
 package com.example.myapplication
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class LoginActivity:AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login_screen)
+        val signupBtn= findViewById<Button>(R.id.loginscreen_signup_btn)
+        val loginBtn=findViewById<Button>(R.id.loginscreen_login_btn)
+        val googleBtn=findViewById<Button>(R.id.loginscreen_googlelogin_btn)
+        signupBtn.setOnClickListener{
+            val intent=Intent(this,SignupActivity::class.java)
+            startActivity(intent)
+
+        }
+
+
     }
 
 }
---
+++ app/src/main/java/com/example/myapplication/SignupActivity.kt
@@ -0,0 +1,13 @@
+package com.example.myapplication
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SignupActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.signup_screen)
+
+    }
+}
