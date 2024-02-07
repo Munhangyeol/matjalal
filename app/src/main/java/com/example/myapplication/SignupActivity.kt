a843ef148e8ccca5a0987647a3cb8bc802edd3b2
Munhangyeol <mhg10181018@gmail.com>
2024-02-07 18:58:29 +0900

    [UI FEAT]:MAIN

--- app/src/main/java/com/example/myapplication/SignupActivity.kt
+++ app/src/main/java/com/example/myapplication/SignupActivity.kt
@@ -2,9 +2,14 @@ package com.example.myapplication
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 
 class SignupActivity: AppCompatActivity() {
+    lateinit var id:EditText
+    lateinit var phoneNumber:EditText
+    lateinit var password:EditText
+    lateinit var secondPassword:EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.signup_screen)
